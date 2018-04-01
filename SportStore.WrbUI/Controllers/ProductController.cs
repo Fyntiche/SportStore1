46b955cb304d47d1bc2825b740c6cbee1894da81
Иван Ерёмов <ivaneremov@gmail.com>
2018-04-01 18:09:09 +0300

    /

---
+++ SportStore.WrbUI/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using SportStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IProductRepository repository;
+        public ProductController(IProductRepository productRepository)
+        {
+            this.repository = productRepository;
+        }
+
+        public ViewResult List()
+        {
+            return View( repository.Products );
+        }
+
+    }
+}
---
+++ SportStore.WrbUI/Infrastructure/NunjectDependencyResolver.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Moq;
+using Ninject;
+using System.Linq;
+using SportStore.Domain.Concrete;
+using SportStore.Domain.Abstract;
+using SportStore.Domain.Entities;
+
+
+namespace SportStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet( serviceType );
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll( serviceType );
+        }
+
+        private void AddBindings()
+        {
+
+            kernel.Bind<IProductRepository>().To<EFProductRepository>();
+
+        }
+    }
+}
